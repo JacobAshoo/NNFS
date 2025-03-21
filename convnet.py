2e70d87bd9500f3f8e8c99a995ab5b39becdddbf
jacob <jacobashoo@gmail.com>
2025-03-20 20:05:19 -0400

    runs on cpu of cuda not available

--- convnet.py
+++ convnet.py
@@ -532,7 +532,7 @@ def train(
 
 
 if __name__ == "__main__":
-    device = torch.device("cuda:0")
+    device = torch.device("cuda:0") if torch.cuda.is_available() else "cpu"
     x, y = import_data()
     train_loader, test_loader = load_data(x, y, device=device)
 
